7d5e58cad58196bfec3c4b4fa2e0bd5d290e02b3
MarcoValdivia094 <a21310094@ceti.mx>
2025-09-18 16:21:50 -0600

    1-Variables.py

---
+++ 1-Variables.py
@@ -0,0 +1,7 @@
+saludo = "Hola mundo" #almacenamos el string en la variable
+print(saludo) #imprimimos la variable con el valor contenido
+
+x = 2 #declaramos la variable 1
+y=2 #declaramos la variable 2
+z=x+y #sumamos las variables previamente creadas y lo almacenamos en la variable z
+print(z) #imprimimos el resultado de la suma
