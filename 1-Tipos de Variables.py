480067e7387fb7ad26a81e3f26a24fcef4d6af4e
MarcoValdivia094 <a21310094@ceti.mx>
2025-09-19 08:31:24 -0600

    Renombrar código y dividir las variables, es decir, crear apartado para cada tipo.

---
+++ 1-Tipos de Variables.py
@@ -0,0 +1,25 @@
+#Esto es un comentario, esta línea del código no se ejecutará, solo sirve como mensaje para el que lea el código
+
+#Ejemplo de una variable tipo String
+  Hola = "Hola mundo" #Almacenamos el string en la variable
+  print(Hola) #Imprimimos la variable con el valor contenido
+
+#Ejemplo de variables tipo int y también una operación con estas.
+  x = 2 #Declaramos la variable 1
+  y = 2 #Declaramos la variable 2
+  z = x + y #Sumamos las variables previamente creadas y lo almacenamos en la variable z
+  print(z) #Imprimimos el resultado de la suma
+
+#Ejemplo de variable tipo float (es decir con punto decimal, aunque tambien puede tener un valor entero)
+  PI = 3.14159
+
+#Ejemplo de variables tipo "Bool", estas solo pueten estar encendidas "True" o apagadas "False"
+  valor_booleano = True #Cabe decir que no puedes escribir "true" o "false" ya que python solo funciona con "True" y "False", es decir con la primera en mayúscula
+  valor_booleano = False
+
+"""
+Este es un comentario
+multilínea de Python.
+Con él, podemos ir
+escribiendo múltiples líneas.
+"""
