42a74627fd31b9d5a6dcac9d2fcb9087aa46d352
MarcoValdivia094 <a21310094@ceti.mx>
2025-09-19 19:03:45 -0600

    Que es Mutabilidad, iteranilidad y Hash

--- I- Mutabilidad, iterabilidad y hashabilidad con Python.py
+++ I- Mutabilidad, iterabilidad y hashabilidad con Python.py
@@ -1,13 +1,13 @@
 # Mutabilidad
-  # Un objeto mutable puede cambiar su contenido después de ser creado
-  lista_mutable = [1, 2, 3]
-  lista_mutable[0] = 99  # Se modifica el primer elemento
-  print("Lista mutable:", lista_mutable)  # [99, 2, 3]
+# Un objeto mutable puede cambiar su contenido después de ser creado
+lista_mutable = [1, 2, 3]
+lista_mutable[0] = 99  # Se modifica el primer elemento
+print("Lista mutable:", lista_mutable)  # [99, 2, 3]
 
 # Inmutabilidad
-  # Un objeto inmutable no puede cambiar su contenido una vez creado
-  tupla_inmutable = (1, 2, 3)
-  # tupla_inmutable[0] = 99  # Esto lanza TypeError: 'tuple' object does not support item assignment
+# Un objeto inmutable no puede cambiar su contenido una vez creado
+tupla_inmutable = (1, 2, 3)
+# tupla_inmutable[0] = 99  # Esto lanza TypeError: 'tuple' object does not support item assignment
 
 """
  Hashabilidad
@@ -25,25 +25,25 @@
 """
 
 # Ver el número hash de un objeto
-  texto = "Marco"
-  print("Hash de texto:", hash(texto))  # Devuelve un número entero único
+texto = "Marco"
+print("Hash de texto:", hash(texto))  # Devuelve un número entero único
 
-  numero = 42
-  print("Hash de número:", hash(numero))  # También funciona con enteros
+numero = 42
+print("Hash de número:", hash(numero))  # También funciona con enteros
 
 # Iterabilidad
   # Un objeto iterable puede ser recorrido elemento por elemento (por ejemplo, en un bucle for)
-  mi_lista = ["a", "b", "c"]
-  for letra in mi_lista:
-      print("Letra:", letra) #Ciclo 0= a, Ciclo 1= b y Ciclo 2= c
+mi_lista = ["a", "b", "c"]
+for letra in mi_lista:
+  print("Letra:", letra) #Ciclo 0= a, Ciclo 1= b y Ciclo 2= c
 
 # También se puede convertir en iterador manualmente
-  iterador = iter(mi_lista)
-  print(next(iterador))  # 'a'
-  print(next(iterador))  # 'b'
-  print(next(iterador))  # 'c'
-  # print(next(iterador))  # StopIteration si se acaba
+iterador = iter(mi_lista)
+print(next(iterador))  # 'a'
+print(next(iterador))  # 'b'
+print(next(iterador))  # 'c'
+# print(next(iterador))  # StopIteration si se acaba
 
 # Puedes verificar si un objeto es iterable
-  from collections.abc import Iterable
-  print("¿Es iterable?", isinstance(mi_lista, Iterable))  # True
+from collections.abc import Iterable
+print("¿Es iterable?", isinstance(mi_lista, Iterable))  # True
